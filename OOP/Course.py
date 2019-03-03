c1af52d96c6e92089c18b02fc1fd2c34d81454f8
Jrmlmnq <jerome_lominoque@dlsu.edu.ph>
2019-03-03 15:55:42 +0800

    Test

--- OOP/Course.py
+++
@@ -1,5 +0,0 @@
-class Course:
-    def __int__(self, course_id, course_name, section):
-        self.course_id = course_id
-        self.course_name = course_name
-        self.section = section
--- OOP/README.md
+++
@@ -1,1 +0,0 @@
-# Timetabling-problem
