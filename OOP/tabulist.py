4edf527c06dff3348bb5ce6ca661716bf47ba1c4
Jrmlmnq <jerome_lominoque@dlsu.edu.ph>
2019-03-03 15:52:15 +0800

    Improved OOP Implementation

---
+++ OOP/tabulist.py
@@ -0,0 +1,55 @@
+class tabulist:
+    # shoart = short-term tabulist
+    # long = long-term tabulist
+    def __init__(self):
+        self.short = []
+        self.long = []
+        self.shortLen = 5
+
+    def emptyList(self):
+        return self.short == []
+        return self.long == []
+
+    def checkListIsFull(self):
+        if self.sizeShort() > self.shortLen:
+            self.dequeueShort()
+
+    # -------long term function-------
+    def checkLong(self, schedule):
+        for x in range(0, len(self.long)):
+            if self.long[x] == schedule:
+                return False
+        return True
+
+    def enqueueLong(self, schedule):
+        self.long.insert(0, schedule)
+        self.checkListIsFull()
+
+    def dequeueLong(self):
+        return self.long.pop()
+
+    # --------short term function-------
+    def checkshort(self, schedule):
+        for x in range(0, len(self.short)):
+            if self.short[x] == schedule:
+                return False
+        return True
+
+    def enqueueShort(self, schedule):
+        self.short.insert(0, schedule)
+
+    def dequeueShort(self):
+        return self.short.pop()
+
+    def sizeShort(self):
+        return len(self.short)
+
+    # -------------------------Check tabulist element-------------------
+
+    def print_all(self):
+        print("Short Term")
+        for x in self.short:
+            print(x.print_all())
+        print("Long Term")
+        for x in self.long:
+            print(x.print_all())
---
+++ OOP/tester.py
@@ -0,0 +1,77 @@
+from Classroom import*
+from TimeSchedule import*
+from tabulist import*
+from DBconnect import*
+from DataManipulator import*
+
+tabu_list = tabulist()
+DBConnect = DBconnect()
+DM = DataManipulator()
+
+classified_rooms = DBConnect.getRoomsFromDB()
+##print(classified_rooms)
+Lecs = DM.InstantiateMultipleClassrooms(classified_rooms[0])
+Labs = DM.InstantiateMultipleClassrooms(classified_rooms[1])
+
+print(Lecs)
+print(Labs)
+
+ClassRoomList = []
+ClassRoomList.extend(Lecs)
+ClassRoomList.extend(Labs)
+
+print(ClassRoomList)
+
+TimeSlot_scheduler = TimeSchedule()
+
+
+course_id = DBConnect.getCourseIdFromDB()
+course_id = DBConnect.getCleanOneTuple(course_id)
+
+List_prof = DBConnect.getProfFromDB()
+List_prof = DBConnect.getCleanOneTuple(List_prof)
+
+Coursecode = DBConnect.getCourseCodeFromDB(course_id)
+Coursecode = DBConnect.getCleanOneTuple(Coursecode)
+#matrix = TimeSlot_scheduler.fill_sched(Coursecode, List_prof)
+#print(matrix)
+
+
+TimeSlot_scheduler.schedule_timetabling(Coursecode, List_prof, tabu_list, ClassRoomList[0])
+
+#sArray = ['BIOINFO', 'Mr. Anish']
+#TimeSlot_scheduler.random_schedule(ClassRoomList, 'Bean')
+#sArray2 = ['COMPRO2' , 'S. Alain']
+#sArray3 = ['COMPRO', 'Ms. Tessie']
+#sArray4 = ['MACLERN', 'Ms.Courmtney']
+#sArray5 = ['ADVDISC', 'S. Duke']
+#SubMatrix = [sArray, sArray2, sArray3, sArray4, sArray5]
+#SubMatrix2 = SubMatrix
+#MainMatrix = []
+#MainMatrix.append(SubMatrix)
+#sArray = ['WEBAPDE', 'S. Stephen']
+#sArray2 = ['MOBAPDE' , 'S. Miguel']
+#sArray3 = ['TREDONE', 'Ms. Pia']
+#SubMatrix = [sArray, sArray2, sArray3]
+#MainMatrix.append(SubMatrix)
+#MainMatrix.append(SubMatrix2)
+#MainMatrix.append(SubMatrix)
+
+
+
+
+#print(len(TimeSlot_scheduler.course_id))
+
+#TimeSlot_scheduler.random_schedule(ClassRoomList, 'Bean')
+#print(ClassRoomList[0].get_monday_time()[0].get_prof())
+#print(ClassRoomList[0].get_monday_time()[1].get_prof())
+#print(ClassRoomList[0].get_monday_time()[2].get_prof())
+#print(ClassRoomList[0].get_monday_time()[3].get_prof())
+
+#print(ClassRoomList[0].get_monday_time()[0].get_course())
+#print(ClassRoomList[0].get_monday_time()[1].get_course())
+
+# ------------------------Tips------------------------
+# access get monday timeslot index 0 timeslot get prof
+# print(C1.get_monday_time()[0].get_prof())
+# print(ClassRoomList[0].get_monday_time()[0].get_time())
